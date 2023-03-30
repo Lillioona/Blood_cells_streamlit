c052318e7a889bceb25542d0f07bf9cf3415b682
AmrithaPrasneh <125680019+AmrithaPrasneh@users.noreply.github.com>
2023-03-30 06:51:56 +0200

    Update 23_blood_cells_streamlit_app.py

--- 23_blood_cells_streamlit_app.py
+++ 23_blood_cells_streamlit_app.py
@@ -171,16 +171,13 @@ if selected == 'Prediction':
             st.subheader("Select the image for prediction")
             # load model
             if(model == 'ResNet'):
-                model = load_model('models/Best_model_ft_5th_layer.h5', custom_objects={'f1':f1})
-               # model = load_model('/Users/prasnehpuzhakkal/Downloads/Best_model_ft_5th_layer.h5', 
-                                  # custom_objects={'f1':f1})
+                model = tf.keras.models.load_model('models/Best_model_ft_5th_layer.h5', custom_objects={'f1':f1})
+               
             elif(model =='ResNet_MixedInput'):
-                model = load_model('models/final_mixed_input_model_ft_no_bpc.h5', custom_objects={'f1':f1})
-                #model = load_model('/Users/prasnehpuzhakkal/Downloads/final_mixed_input_model_ft_no_bpc.h5', 
-                                  # custom_objects={'f1':f1}) 
+                model = tf.keras.models.load_model('models/final_mixed_input_model_ft_no_bpc.h5', custom_objects={'f1':f1})
+                
             elif(model =='VGG16'):
-                model = load_model('models/vgg16_augmented_model.h5')
-                #model = load_model('/Users/prasnehpuzhakkal/Downloads/vgg16_augmented_model.h5')
+                model = tf.keras.models.load_model('models/vgg16_augmented_model.h5')
 
             #placeholder = st.empty()
             #with placeholder.container():
