9b73c0a864d080e086227b31bcfdaff63ede5e1f
RoadieHouse <120097990+RoadieHouse@users.noreply.github.com>
2023-03-31 12:44:21 +0200

    Update 23_blood_cells_streamlit_app.py

--- 23_blood_cells_streamlit_app.py
+++ 23_blood_cells_streamlit_app.py
@@ -128,7 +128,7 @@ In this study the focus lies on erythroblasts which are an early stage of erythr
 if selected == 'E.D.A.':
     st.header('Exploratory Data Analysis')
     
-    df = pd.read_csv("bcc_num_data.csv", encoding='utf-8')
+    df = pd.read_csv("bcc_num_data.csv", encoding='iso-8859-1')
     st.dataframe(df)
     
     
