a4571c6a5db02bcf4a821ecea68733a13387dd52
RoadieHouse <120097990+RoadieHouse@users.noreply.github.com>
2023-04-01 18:09:03 +0200

    Update 23_blood_cells_streamlit_app.py

--- 23_blood_cells_streamlit_app.py
+++ 23_blood_cells_streamlit_app.py
@@ -209,7 +209,7 @@ CLASS_LABELS = ['basophil',
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-    #model_file = BytesIO(requests.get(MODEL_URL).content)
+    model_file = BytesIO(requests.get(MODEL_URL).content)
     model = tf.keras.models.load_model(MODEL)
     return model
 
