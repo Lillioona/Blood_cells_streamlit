496b83fcc29354ba73975538b34ea0d036acbb58
RoadieHouse <120097990+RoadieHouse@users.noreply.github.com>
2023-04-02 01:15:10 +0200

    Update 23_blood_cells_streamlit_app.py

--- 23_blood_cells_streamlit_app.py
+++ 23_blood_cells_streamlit_app.py
@@ -301,8 +301,7 @@ if selected == 'Modelisation':
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 # Necessary function and variables
-MODEL_URL = "https://github.com/Lillioona/Blood_cells_streamlit/blob/main/models/vgg16_augmented_model.h5"
-MODEL = "Best_model_ft_5th_layer.h5"
+MODEL_URL = "https://www.dropbox.com/s/hztf508d1b44ywu/Best_model_ft_5th_layer.h5?dl=1"
 
 IMG_SIZE = (360,360) 
 
@@ -317,8 +316,8 @@ CLASS_LABELS = ['basophil',
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-    #model_file = BytesIO(requests.get(MODEL_URL).content)
-    model = tf.keras.models.load_model(MODEL_URL)
+    model_file = BytesIO(requests.get(MODEL_URL).content)
+    model = tf.keras.models.load_model(model_file)
     return model
 
 #load the model to use for predictions
