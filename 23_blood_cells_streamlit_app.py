f77eea85acca79094f3a68a2942421508e5b8093
RoadieHouse <120097990+RoadieHouse@users.noreply.github.com>
2023-04-01 13:08:50 +0200

    Update 23_blood_cells_streamlit_app.py

--- 23_blood_cells_streamlit_app.py
+++ 23_blood_cells_streamlit_app.py
@@ -24,7 +24,7 @@ st.set_page_config(page_title="BCC", page_icon=":drop_of_blood:", layout="center
 #------------------------------------------------------------------------------------------------------------------------------------------ 
 # open images to display (maybe add them to corresponding section for clarity/structure
 img_home_01 = Image.open('images/Blood_cell_examples.png')
-img_EDA_01 = Image.open('images/EDA_01.png')
+img_EDA_01 = Image.open('images/Image_size.png')
 img_EDA_02 = Image.open('images/EDA_02.png')
 img_EDA_03 = Image.open('images/EDA_03.png')
 Analysis_01 = Image.open('images/Analysis_01.png')
