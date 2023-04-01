b94fbabffdd593531ce76b04d19bfc41f40f9782
RoadieHouse <120097990+RoadieHouse@users.noreply.github.com>
2023-04-02 00:08:41 +0200

    Update 23_blood_cells_streamlit_app.py

--- 23_blood_cells_streamlit_app.py
+++ 23_blood_cells_streamlit_app.py
@@ -164,7 +164,29 @@ if selected == 'E.D.A.':
     The image size varied between the different datasets, as displayed in the following scatter plot. They were all resized to 360x360 for the 
                 continuous process.
                 """)
-    st.image(img_EDA_01)
+    #st.image(img_EDA_01)
+    
+    # set plot style
+    sns.set_style('darkgrid')
+
+    # create plot
+    figure, axis = plt.subplots(figsize=(8,8), facecolor="#0e1117")
+    axis.set_facecolor("#0e1117")
+    axis.set_xlim([351,599])
+    axis.set_ylim([351,599])
+    axis.set_xlabel('Width', color='white')
+    axis.set_ylabel('Height', color='white')
+    axis.spines['top'].set_visible(False)
+    axis.spines['right'].set_visible(False)
+    axis.set_title("Original image resolution", fontdict={'color': "white"}, size= 18, pad=25)
+    axis.tick_params(colors="white", bottom=True, left=True)
+    ax = sns.scatterplot(data=df, x='width', y='height',  hue='origin',
+                        palette='deep', size= 'origin', sizes=(100, 200))
+    axis.legend(loc=(0.125,.82), frameon=True, fontsize="large")
+
+    # display plot in Streamlit
+    st.pyplot(figure)
+    
     
     st.subheader('Brightness')
     st.markdown("""
