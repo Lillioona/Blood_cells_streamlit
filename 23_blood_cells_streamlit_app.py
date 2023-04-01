2b3560178b113764e7f19bf09731c97e2d88ac56
Lillioona <125668256+Lillioona@users.noreply.github.com>
2023-04-01 21:04:10 +0100

    Update 23_blood_cells_streamlit_app.py

--- 23_blood_cells_streamlit_app.py
+++ 23_blood_cells_streamlit_app.py
@@ -114,7 +114,7 @@ if selected == 'E.D.A.':
     st.header('Exploratory Data Analysis')
     st.markdown(
         """
-        Three different publicly available datasets were used to achieve this project's objective. The total volume of these datasets was 52,000 images. 
+        Three different open source datasets were used to achieve this project's objective. The total volume of these datasets was 52,000 images. 
          The classification into one of these eight classes was the target of this project’s model. 
         """
     )
