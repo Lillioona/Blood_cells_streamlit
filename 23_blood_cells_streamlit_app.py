0f5dee702c841e1c4da47225a09b0b28c78f1593
RoadieHouse <120097990+RoadieHouse@users.noreply.github.com>
2023-04-02 03:50:57 +0200

    Update 23_blood_cells_streamlit_app.py

--- 23_blood_cells_streamlit_app.py
+++ 23_blood_cells_streamlit_app.py
@@ -170,7 +170,7 @@ if selected == 'E.D.A.':
                 continuous process.
                 """)
    # Create scatterplot with Plotly
-    fig = px.scatter(df, x='Width', y='Height', color='Origin', size='Origin', symbol='Origin',
+    fig = px.scatter(df, x='Width', y='Height', color='Origin', size=5, symbol='Origin',
                      hover_data={'Shape': True, 'Luminosity' : True, 'Brightness' : True},
                      hover_name="Origin")
 
